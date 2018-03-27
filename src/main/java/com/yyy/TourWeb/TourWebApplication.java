395ebf785af62bed1a5208467ef22f3389e5c7c5
小王子(基鸿) <976228284@qq.com>
2018-03-27 18:22:54 +0800

    配置redis

--- src/main/java/com/yyy/TourWeb/TourWebApplication.java
+++ src/main/java/com/yyy/TourWeb/TourWebApplication.java
@@ -2,11 +2,53 @@ package com.yyy.TourWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.core.publisher.Mono;
 
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.badRequest;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+/**
+ * 主类
+ *
+ * @author yyy
+ */
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
+@EnableRedisRepositories
 public class TourWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TourWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TourWebApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<?> routes(ReactiveRedisTemplate<String, String> redisTemplate) {
+        return nest(path("/api"),
+
+                route(GET("/get/{key}"), request -> {
+                    Mono<String> v = redisTemplate.opsForValue().get(request.pathVariable("key"));
+                    return ok().body(v, String.class);
+                }).andRoute(GET("/set/{key}"), request -> {
+                    String k = request.pathVariable("key");
+
+                    return request.attribute("v").map(o -> {
+                                redisTemplate.opsForValue().set(k, o.toString());
+                                return ok().build();
+                            }).orElse(badRequest().build());
+                })
+        );
+    }
 }
---
+++ src/main/java/com/yyy/TourWeb/config/ReactiveRedisConfig.java
@@ -0,0 +1,37 @@
+package com.yyy.TourWeb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class ReactiveRedisConfig {
+
+    @Primary
+    @Bean
+    public ReactiveRedisConnectionFactory lettuceConnectionFactory() {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(2))
+                .shutdownTimeout(Duration.ZERO)
+                .build();
+
+        RedisStandaloneConfiguration standalone = new RedisStandaloneConfiguration("119.23.149.25", 6379);
+        standalone.setPassword(RedisPassword.of("daedalus"));
+
+        return new LettuceConnectionFactory(standalone, clientConfig);
+    }
+
+    @Bean
+    ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
+    }
+}
