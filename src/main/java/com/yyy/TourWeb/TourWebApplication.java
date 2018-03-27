ba011454b4d0097d49bf1e6febd4446930eefb41
小王子(基鸿) <976228284@qq.com>
2018-03-27 15:10:26 +0800

    first commit

---
+++ src/main/java/com/yyy/TourWeb/TourWebApplication.java
@@ -0,0 +1,12 @@
+package com.yyy.TourWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yyy/TourWeb/TourWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yyy.TourWeb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TourWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
